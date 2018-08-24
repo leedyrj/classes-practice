70ed5356c723886cdd59e2b340255d612002ab85
Robert Leedy <leedyrj@gmail.com>
2018-08-24 14:43:59 -0500

    moved company and employees classes into own files to practice modularity

---
+++ Employee.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace classes
+{
+    public class Employees
+    {
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime startDate { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,49 +3,6 @@ using System.Collections.Generic;
 
 namespace classes
 {
-
-    public class Employees
-    {
-        public string firstName { get; set; }
-
-        public string lastName { get; set; }
-
-        public string Title { get; set; }
-
-        public DateTime startDate { get; set; }
-    }
-    public class Company
-    {
-
-        // Some readonly properties (let's talk about gets, baby)
-        public string Name { get; }
-        public DateTime CreatedOn { get; }
-
-        // Create a public property for holding a list of current employees
-        public List<Employees> employeeList = new List<Employees>();
-
-        /*
-            Create a constructor method that accepts two arguments:
-                1. The name of the company
-                2. The date it was created
-
-
-            The constructor will set the value of the public properties
-        */
-        public Company(string companyName, DateTime companyCreated)
-        {
-            Name = companyName;
-            CreatedOn = companyCreated;
-        }
-
-        public void addEmployee(Employees employee)
-        {
-            employeeList.Add(employee);
-        }
-    }
-
-
-
     class Program
     {
         static void Main(string[] args)
